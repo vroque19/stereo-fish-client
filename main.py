6784239e85c77bd3a92c6439faea5dd1eb3f281d
vroque19 <viroque19@gmail.com>
2025-04-22 15:41:20 -0700

    save frame to upload

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 import time
 import os
+import sys
+import termios
+import tty
+import select
 # from dotenv import load_dotenv
 from lcd import init_display, show_welcome
 from cam import stream
@@ -10,7 +14,14 @@ from cam import stream
 def main():
     disp = init_display()
     show_welcome(disp)
-    stream(disp)
+    # Setup terminal to read input without Enter
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    tty.setcbreak(sys.stdin.fileno())
+    try:
+        stream(disp, fd, old_settings)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
 
 if __name__ == "__main__":
